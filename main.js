4aae0bdca605c2600d761d6bd2145adb9215d9f8
k.trolp@gmx.at <nii@gmx.at>
2020-12-13 12:21:31 +0100

    Change Comment

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 
-// Load modules:
+// Load modules
 const utils = require('@iobroker/adapter-core');
 const fetch = require('fetch');
 
