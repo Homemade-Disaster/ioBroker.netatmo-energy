eb049e0161e760b2f456eeb2e151cae1d2ee7221
k.trolp@gmx.at <nii@gmx.at>
2020-12-13 18:24:03 +0100

    Translation finished

--- admin/words.js
+++ admin/words.js
@@ -16,158 +16,146 @@ systemDictionary = {
 	},
 	'HomeId': {
 		'en': 'Home-ID:',
-		'de': 'Home-ID:'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Home-ID:',
+		'ru': 'Домашний-ID:',
+		'pt': 'ID de casa:',
+		'nl': 'Huis-ID:',
+		'fr': 'Identifiant du domicile:',
+		'it': 'ID casa:',
+		'es': 'ID de casa:',
+		'pl': 'ID domu:',
+		'zh-cn': '住家编号：'
 	},
-	/*	'option2': {
-		'en': 'option2',
-		'de': 'Option 2',
-		'ru': 'option2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
-	}*/
 	'ClientId': {
 		'en': 'Client-ID:',
-		'de': 'Client-ID:'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Client-ID:',
+		'ru': 'ID клиента:',
+		'pt': 'ID do Cliente:',
+		'nl': 'Klant identificatie:',
+		'fr': 'Identité du client:',
+		'it': 'Identificativo cliente:',
+		'es': 'Identificación del cliente:',
+		'pl': 'Identyfikator klienta:',
+		'zh-cn': '客户编号：'
 	},
 	'ClientSecretID': {
 		'en': 'Client-Secret-ID:',
-		'de': 'Client-Secret-ID:'
-	/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Client-Secret-ID:',
+		'ru': 'Опция 1',
+		'pt': 'Client-Secret-ID:',
+		'nl': 'Client-geheim-ID:',
+		'fr': 'Client-Secret-ID:',
+		'it': 'ID segreto client:',
+		'es': 'ID secreto del cliente:',
+		'pl': 'Client-Secret-ID:',
+		'zh-cn': '客户机密ID：',
 	},
 	'User': {
 		'en': 'Username:',
-		'de': 'Benutzername:'
-	/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Benutzername:',
+		'ru': 'Имя пользователя:',
+		'pt': 'Nome do usuário:',
+		'nl': 'Gebruikersnaam:',
+		'fr': 'Nom d utilisateur:',
+		'it': 'Nome utente:',
+		'es': 'Nombre de usuario:',
+		'pl': 'Nazwa Użytkownika:',
+		'zh-cn': '用户名：'
 	},
 	'Password': {
 		'en': 'Password:',
-		'de': 'Passwort:'
-	/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Passwort:',
+		'ru': 'Пароль:',
+		'pt': 'Senha:',
+		'nl': 'Wachtwoord:',
+		'fr': 'Mot de passe:',
+		'it': 'Parola d'ordine:',
+		'es': 'Contraseña:',
+		'pl': 'Hasło:',
+		'zh-cn': '密码：'
 	},
 	'Home ID ihrer Netatmo Energy Installation': {
 		'en': 'Home ID from your Netatmo Energy installation',
-		'de': 'Home ID ihrer Netatmo Energy Installation'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Home ID ihrer Netatmo Energy Installation',
+		'ru': 'Домашний идентификатор из вашей установки Netatmo Energy',
+		'pt': 'Home ID da sua instalação Netatmo Energy',
+		'nl': 'Home ID van uw Netatmo Energy-installatie',
+		'fr': 'Identifiant du domicile de votre installation Netatmo Energy',
+		'it': 'ID casa dalla tua installazione Netatmo Energy',
+		'es': 'Home ID de su instalación de Netatmo Energy',
+		'pl': 'Identyfikator domu z instalacji Netatmo Energy',
+		'zh-cn': 'Netatmo Energy安装中的家庭ID'
 	},
 	'Client ID ihrer Netatmo APP': {
 		'en': 'Client ID from your Netatmo APP',
-		'de': 'Client ID ihrer Netatmo APP'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Client ID ihrer Netatmo APP',
+		'ru': 'Идентификатор клиента из вашего приложения Netatmo',
+		'pt': 'ID de cliente de seu aplicativo Netatmo',
+		'nl': 'Client-ID van uw Netatmo APP',
+		'fr': 'ID client de votre APP Netatmo',
+		'it': 'ID client dalla tua APP Netatmo',
+		'es': 'ID de cliente de su aplicación Netatmo',
+		'pl': 'Identyfikator klienta z aplikacji Netatmo',
+		'zh-cn': '来自Netatmo APP的客户端ID'
 	},
 	'Client Secret ID ihrer Netatmo APP': {
 		'en': 'Client Secret ID from your Netatmo APP',
-		'de': 'Client Secret ID ihrer Netatmo APP'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Client Secret ID ihrer Netatmo APP',
+		'ru': 'Идентификатор секрета клиента из вашего приложения Netatmo',
+		'pt': 'ID secreta do cliente de seu aplicativo Netatmo',
+		'nl': 'Client Secret ID van uw Netatmo APP',
+		'fr': 'Identifiant secret client de votre application Netatmo',
+		'it': 'ID segreto client dalla tua APP Netatmo',
+		'es': 'ID secreto del cliente de su aplicación Netatmo',
+		'pl': 'Tajny identyfikator klienta z aplikacji Netatmo',
+		'zh-cn': '来自Netatmo APP的客户端密钥ID'
 	},
 	'Benutzername von ihrem Netatmo Konto': {
 		'en': 'Username from your Netatmo Account',
-		'de': 'Benutzername von ihrem Netatmo Konto'
-		/*		'ru': 'Опция 1',
+		'de': 'Benutzername von ihrem Netatmo Konto',
+		'ru': 'Опция 1',
 		'pt': 'Opção 1',
 		'nl': 'Optie 1',
 		'fr': 'Option 1',
 		'it': 'opzione 1',
 		'es': 'Opción 1',
 		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'zh-cn': '选项1'
 	},
 	'Passwort von ihrem Netatmo Konto': {
 		'en': 'Password from your Netatmo Account',
-		'de': 'Passwort von ihrem Netatmo Konto'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Passwort von ihrem Netatmo Konto',
+		'ru': 'Имя пользователя из вашей учетной записи Netatmo',
+		'pt': 'Nome de usuário da sua conta Netatmo',
+		'nl': 'Gebruikersnaam van uw Netatmo-account',
+		'fr': 'Nom d utilisateur de votre compte Netatmo',
+		'it': 'Nome utente dal tuo account Netatmo',
+		'es': 'Nombre de usuario de su cuenta Netatmo',
+		'pl': 'Nazwa użytkownika z konta Netatmo',
+		'zh-cn': '您的Netatmo帐户的用户名'
 	},
 	'Allgemeine Einstellungen': {
 		'en': 'General settings',
-		'de': 'Allgemeine Einstellungen'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'Allgemeine Einstellungen',
+		'ru': 'Общие настройки',
+		'pt': 'Configurações Gerais',
+		'nl': 'Algemene instellingen',
+		'fr': 'Réglages généraux',
+		'it': 'Impostazioni generali',
+		'es': 'Configuración general',
+		'pl': 'Ustawienia główne',
+		'zh-cn': '常规设置'
 	},
 	'applyimmediately': {
 		'en': 'apply immediately',
-		'de': 'sofort aktualisieren'
-		/*		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'*/
+		'de': 'sofort aktualisieren',
+		'ru': 'применить немедленно',
+		'pt': 'aplique imediatamente',
+		'nl': 'onmiddellijk toepassen',
+		'fr': 'appliquer immédiatement',
+		'it': 'applicare immediatamente',
+		'es': 'aplicar inmediatamente',
+		'pl': 'zastosuj natychmiast',
+		'zh-cn': '立即申请'
 	}
 };
